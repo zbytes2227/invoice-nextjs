2e439e17ca36179353324feda540292d06bd7169
zbytes2227 <zbytes0@gmail.com>
2024-03-11 19:04:24 +0530

    firstsa

--- app/admin/invoices/add/page.js
+++ app/admin/invoices/add/page.js
@@ -133,7 +133,7 @@ const Page = () => {
             for="Contact"
             class="block mb-2 text-sm font-medium text-gray-900"
           >
-            Tax
+            Tax(%)
           </label>
           <input
             id="Contact"
